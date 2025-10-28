147af549089772463701c5075480487a56134811
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-10-28 13:04:23 -0700

    made new files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/their/Documents/GitHub/connect-4-starter
+# Build directory: C:/Users/their/Documents/GitHub/connect-4-starter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ classes/Connect4.cpp
@@ -0,0 +1,1 @@
+#include "Connect4.h"
