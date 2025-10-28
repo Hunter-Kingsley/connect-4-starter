8d2ce3bf16a0adbc403687c4cf1af5a19ccef249
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-10-28 15:03:57 -0700

    started connect 4

--- classes/Connect4.cpp
+++ classes/Connect4.cpp
@@ -1,1 +1,110 @@
 #include "Connect4.h"
+
+Connect4::Connect4() 
+{
+    _grid = new Grid(7, 6);
+}
+
+Connect4::~Connect4()
+{
+    delete _grid;
+}
+
+void Connect4::setUpBoard()
+{
+    setNumberOfPlayers(2);
+    _gameOptions.rowX = 7;
+    _gameOptions.rowY = 6;
+    _grid->initializeSquares(80, "square.png");
+
+    Player* blackPlayer = getPlayerAt(RED_PLAYER);
+    Player* whitePlayer = getPlayerAt(YELLOW_PLAYER);
+
+    if (gameHasAI()) {
+        setAIPlayer(AI_PLAYER);
+    }
+
+    startGame();
+}
+
+Player* Connect4::checkForWinner()
+{
+    return nullptr;
+}
+
+bool Connect4::checkForDraw()
+{
+    return false;
+}
+
+std::string Connect4::initialStateString()
+{
+    return " ";
+}
+
+std::string Connect4::stateString()
+{
+    return " ";
+}
+
+void Connect4::setStateString(const std::string &s)
+{
+
+}
+
+bool Connect4::actionForEmptyHolder(BitHolder &holder)
+{
+    if (holder.bit()) return false;
+
+    ChessSquare* square = static_cast<ChessSquare*>(&holder);
+    int x = square->getColumn();
+    int y = square->getRow();
+    Player* currentPlayer = getCurrentPlayer();
+
+    if (_grid->getSquare(0, 0)->bit()) {
+        Bit* newPiece = createPiece(currentPlayer);
+        newPiece->setPosition(holder.getPosition());
+        holder.setBit(newPiece);
+    }
+
+
+    return false;
+}
+
+bool Connect4::canBitMoveFrom(Bit &bit, BitHolder &src)
+{
+    return false;
+}
+
+bool Connect4::canBitMoveFromTo(Bit &bit, BitHolder &src, BitHolder &dst)
+{
+    return false;
+}
+
+void Connect4::stopGame()
+{
+
+}
+
+void Connect4::updateAI() 
+{
+
+}
+
+Bit * Connect4::createPiece(Player* player)
+{
+    Bit* bit = new Bit();
+    bit->LoadTextureFromFile(player == getPlayerAt(RED_PLAYER) ? "red.png" : "yellow.png");
+    bit->setOwner(player);
+    return bit;
+}
+
+Player* Connect4::ownerAt(int index) const
+{
+    return nullptr;
+}
+
+int Connect4::negamax(std::string& state, int depth, int playerColor)
+{
+    return 0;
+}
--- classes/Connect4.h
+++ classes/Connect4.h
@@ -1,1 +1,35 @@
-#include <iostream>
+#pragma once
+#include "Game.h"
+
+class Connect4 : public Game 
+{
+public:
+    Connect4();
+    ~Connect4();
+
+    // set up the board
+    void        setUpBoard() override;
+
+    Player*     checkForWinner() override;
+    bool        checkForDraw() override;
+    std::string initialStateString() override;
+    std::string stateString() override;
+    void        setStateString(const std::string &s) override;
+    bool        actionForEmptyHolder(BitHolder &holder) override;
+    bool        canBitMoveFrom(Bit &bit, BitHolder &src) override;
+    bool        canBitMoveFromTo(Bit &bit, BitHolder &src, BitHolder &dst) override;
+    void        stopGame() override;
+
+	void        updateAI() override;
+    bool        gameHasAI() override { return true; }
+    Grid*       getGrid() override { return _grid; }
+private:
+    // Player constants
+    static const int RED_PLAYER = 0;
+    static const int YELLOW_PLAYER = 1;
+    Bit *       createPiece(Player* player);
+    Player*     ownerAt(int index ) const;
+    int         negamax(std::string& state, int depth, int playerColor);
+
+    Grid*       _grid;
+};
